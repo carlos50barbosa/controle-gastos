f907738a12ab6cf41e04ecdd8c529a3a1ae616c4
José Carlos Barbosa <carlos50barbosa@gmail.com>
2025-04-21 03:37:26 -0300

    Atualizacoes 21-04-2025

--- server.cjs
+++ server.cjs
@@ -11,11 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
+
 });
 
 const SECRET = process.env.JWT_SECRET;
